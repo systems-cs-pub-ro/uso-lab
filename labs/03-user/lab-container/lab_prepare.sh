748870cdad77f68dc741fc274ba1e316f5460c5c
Sergiu Weisz <sergiu121@gmail.com>
2024-10-31 00:13:51 +0200

    labs:lab03: Make install arch-agnostic
    Signed-off-by: Sergiu Weisz <sergiu121@gmail.com>

--- labs/03-user/lab-container/lab_prepare.sh
+++ labs/03-user/lab-container/lab_prepare.sh
@@ -3,8 +3,7 @@
 install_docker () {
     sudo apt-get update
     sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    curl -fsSL test.docker.com -o get-docker.sh && sh get-docker.sh
     sudo apt-get update
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
     sudo service docker start
