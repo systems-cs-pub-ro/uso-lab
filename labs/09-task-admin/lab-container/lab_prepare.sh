7340ceb4e98396fe57537f3d5a289a95f66ba121
Matei Stanuca <106833623+mateistanuca1@users.noreply.github.com>
2025-10-29 18:26:28 +0200

    Update labs/09-task-admin/lab-container/lab_prepare.sh
    Co-authored-by: Andreia Ocanoaia <andreia.ocanoaia@gmail.com>

--- labs/09-task-admin/lab-container/lab_prepare.sh
+++ labs/09-task-admin/lab-container/lab_prepare.sh
@@ -11,7 +11,7 @@ install_docker () {
     $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     sudo service docker start
 }
 
